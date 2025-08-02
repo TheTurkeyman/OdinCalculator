afad85515b22adc1ec8b9f2fdc80e937b0e1bec6
TheTurkeyman <donald.mcquacker@gmail.com>
2025-08-02 11:59:45 +0300

    ADD Basic functionality
    All the basic functionality, still works with eval in order to check that everything functions as it supposed to function.

--- script.js
+++ script.js
@@ -10,21 +10,46 @@ const butone = document.querySelector(`#numone`);
 const butzero = document.querySelector(`#numzero`);
 const butplus = document.querySelector(`#plus`);
 const butequal = document.querySelector(`#equal`);
-
-
+const butmultiply = document.querySelector(`#multiply`);
+const dotdec = document.querySelector(`#dotfordecimal`)
 const inputline = document.querySelector('#calcinp');
+const clearbutton = document.querySelector(`#clearbutton`);
+const jsbackbutton = document.querySelector(`#backbutton`);
+const subtraction = document.querySelector(`#minus`);
+const division = document.querySelector(`#divide`);
 
-function inputnumber(clicknum) {
-    inputline.value += `${clicknum}`;
-};
-function plus () {
+function inputnumber(clicknum) 
+{if (clicknum === '.') {
+    const currentInput = inputline.value;
+    const lastOperand = currentInput.split(/[+\-*/]/).pop();
+    if (lastOperand.includes('.')) 
+        {return;}}     
+    inputline.value += `${clicknum}`;} 
+
+
+function plus() {
     inputline.value += `+`;
 };
+
+function multiply() {
+    inputline.value += `*`;
+};
+
+function subtract() {
+    inputline.value += `-`;
+};
+
+function justdivide() {inputline.value += `/`};
+
+
 
 function evaluateInput() {
     const result = eval(inputline.value);
     inputline.value = `${result}`;}
 
+function mehminusone() {inputline.value = inputline.value .slice(0, inputline.value.length -1);}
+
+function resetfunc() {inputline.value = '';};
 
 butnine.addEventListener("click", () => inputnumber(9));
 buteight.addEventListener("click", () => inputnumber(8));
@@ -36,12 +61,20 @@ butthree.addEventListener("click", () => inputnumber(3));
 buttwo.addEventListener("click", () => inputnumber(2));
 butone.addEventListener("click", () => inputnumber(1));
 butzero.addEventListener("click", () => inputnumber(0));
+dotdec.addEventListener("click", () => inputnumber(`.`));
 
-document.addEventListener("keydown", (e) => {const key = e.key;        
-    if (/[0-9+\-*/.=]/.test(key) || key === 'Enter') 
-        {inputnumber(key === 'Enter' ? '=' : key);}
-}); 
+clearbutton.addEventListener('click', () => resetfunc());
 
+document.addEventListener("keydown", (e) => {const key = e.key;
+         if (key === '=' || key === 'Enter') 
+            {evaluateInput();} 
+         else if (key === 'Backspace') {
+        mehminusone();}
+         else if (/[0-9+\-*/.]/.test(key)) 
+            {inputnumber(key);}}); 
+jsbackbutton.addEventListener(`click`, () => mehminusone());
 butplus.addEventListener("click", () => plus());
-
 butequal.addEventListener("click", evaluateInput);
+butmultiply.addEventListener("click", () =>  multiply());
+subtraction.addEventListener('click', () => subtract());
+division.addEventListener('click', () => justdivide())
